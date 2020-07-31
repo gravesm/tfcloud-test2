fe83c479959181076d889b3c955bc75f53bc3909
Mike Graves <mgraves@mit.edu>
2020-07-31 09:30:50 -0400

    init

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "remote" {
+    organization = "gravesmj"
+    workspaces {
+      prefix = "remote-test-"
+    }
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+data "terraform_remote_state" "foo" {
+  backend = "remote"
+
+  config = {
+    organization = "gravesmj"
+    workspaces = {
+      name = "mgraves-${var.workspace}"
+    }
+  }
+}
+
+resource "aws_iam_user" "default" {
+  name = "tf-remote-test-${var.workspace}"
+  tags = {
+    foo = data.terraform_remote_state.foo.name
+  }
+}
